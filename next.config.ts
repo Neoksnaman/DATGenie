4d517eb5b20c582308d826c3f449b6fb2d562881
Neoksnaman <christianbaccay11@gmail.com>
2025-09-17 13:29:11 +0800

    Updated from Firebase as of Sept 17, 2025

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,25 @@
-import type { NextConfig } from 'next';
+import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'placehold.co',
+        port: '',
         pathname: '/**',
-      },
+      }
     ],
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.pdf$/i,
-      type: 'asset/source', // or 'asset/resource'
-    });
-    return config;
   },
 };
 
 export default nextConfig;
+
+
